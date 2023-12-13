1e4f19e3d5eb4d21b3e8b5d0c2cfc9cef1f22b63
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-12-13 13:22:41 -0500

    test

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return <h1>Test Test</h1>;
 }
 
 export default App;
