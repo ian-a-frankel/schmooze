a37f7082c955178189ade89518b418d960205844
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-12-21 11:09:57 -0500

    btn

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -3,8 +3,8 @@
 
 function Search({setNameSearchText}) {
     return (
-        <div className="searchbar">
-          <label>Search Users:</label>
+        <div >
+          <label id='searchbar' >Search Users:</label>
             
           <input onChange={(e)=> setNameSearchText(e.target.value)}  type="text" id="search" placeholder="Type username" />
         </div>
